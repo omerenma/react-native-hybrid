e1bee9f617ef2e9033addac8ee0b30f13252b7ed
omerenma <godwin2341@gmail.com>
2022-07-18 16:18:42 +0100

    configure drawer navigation

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
--- src/Screens/Home/Dashboard.js
+++ src/Screens/Home/Dashboard.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {View, Text} from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+function FirstComponent() {
+  return (
+    <View>
+      <Text>First Drawer Component</Text>
+    </View>
+  );
+}
+
+function SecondComponent() {
+  return (
+    <View>
+      <Text>Second Drawer Component</Text>
+    </View>
+  );
+}
 
 export const Dashboard = () => {
   return (
-    <View>
-        <Text>Dashboard</Text>
-    </View>
-  )
-}
+    <Drawer.Navigator>
+      <Drawer.Screen name="FirstComponent" component={FirstComponent} />
+      <Drawer.Screen name="SecondComponent" component={SecondComponent} />
+    </Drawer.Navigator>
+  );
+};
